994da5c8a375c4416a9a33b2a4df5d7533f84d36
eastwest9 <dong092276@gmail.com>
2025-01-05 19:22:41 +0900

    :sparkles: Domain init + Util init

--- src/main/java/com/springboot/mealkart/domain/ProductDetail.java
+++ src/main/java/com/springboot/mealkart/domain/ProductDetail.java
@@ -1,4 +1,15 @@
 package com.springboot.mealkart.domain;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name = "TB_PRODUCT_DETAIL")
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductDetail {
 }
